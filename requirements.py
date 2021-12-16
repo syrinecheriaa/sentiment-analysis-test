4b81f0b2c6daadb10d9cf544f3e53d0f7164141c
Syrine <scheriaa@invest-insight.ai>
2021-12-16 17:19:38 +0100

    add requirements file

---
+++ requirements.py
@@ -0,0 +1,12 @@
+'pandas==1.1.1',
+'transformers==4.12.5',
+'sentencepiece',
+'protobuf',
+'torch@https://download.pytorch.org/whl/cpu/torch-1.5.0%2Bcpu-cp37-cp37m-linux_x86_64.whl', 
+'pandas',
+'matplotlib',
+'langdetect',
+'numpy',
+'scikit-learn'
+
+
